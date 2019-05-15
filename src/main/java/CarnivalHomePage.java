c2e6dbe0dbeabfc3937c9c8990c4d91609a5e70d
Carlos Rios <carlos.rios@avaldigitallabs.com>
2019-05-14 19:30:44 -0500

    Initial Setup

---
+++ src/main/java/CarnivalHomePage.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class CarnivalHomePage {
+
+    WebDriver driver;
+
+}
